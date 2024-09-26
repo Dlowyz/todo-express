2b714456577c67c1af888ee7d98fe6671c68f197
Dlowyz <artur.lomp@voco.ee>
2024-09-26 10:32:57 +0300

    todo-express

--- index.js
+++ index.js
@@ -38,29 +38,94 @@ app.get('/delete-task/:taskId', (req, res) => {
     })
 })
 
+app.get('/edit-task/:taskId', (req, res) => {
+    let red = req.params.taskId
+    
+    readFile('./tasks.json')
+    .then((tasks) =>{
+
+        res.render('index',{tasks: tasks, error: null, red: red} )
+    }) 
+    
+})
+
+app.get('/delete-tasks/', (req, res) => {
+        console.log('delete all tasks')
+        
+        data = JSON.stringify([], null, 2)
+        fs.writeFile('./tasks.json', data, (err) => {
+            if (err){
+                console.log(err);
+                return;
+            } 
+            res.redirect('/')
+
+        
+        })
+})
+
+
+
 app.get('/', (req, res) =>{
     readFile('./tasks.json')
     .then((tasks) =>{
 
-        res.render('index',{tasks: tasks} )
+        res.render('index',{tasks: tasks, error: null, red: null} )
     }) 
     
         
 
     })
 
+
     
 
-
 app.post('/', (req, res) =>{
-
-    readFile('./tasks.json').then((tasks) => {
-        let index
+        let error = null
+        if (req.body.action == "edit"){
+            console.log(req.body.task)
+            console.log(parseInt(req.body.taskId))
+            readFile('./tasks.json').then(tasks => {
+                tasks.forEach((taskItem) => {
+                    if (taskItem.id == req.body.taskId) {
+                        taskItem.task = req.body.task;
+                    }
+                })
+                const data = JSON.stringify(tasks, null, 2);
+            fs.writeFile('./tasks.json', data, (err) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                res.redirect('/');
+            })
+        })
+        } else if (req.body.action == "Add Task"){
+        if (req.body.task.trim().length === 0){
+            
+            error = 'Task cannot be empty'
+            console.log(error)
+            readFile('./tasks.json')
+            .then(tasks => {
+            res.render('index', {
+                tasks: tasks,
+                error: error,
+                red: null
+            })
+        })
+            
+        } else {
+            console.log('no error')
+        readFile('./tasks.json')
+        .then(tasks => {
+            let index
         if (tasks.length === 0){
             index = 1
+            console.log('2')
         } else {
-            index = tasks[tasks.length - 1].id + 1;
-        }
+            console.log('3')
+            index = tasks[tasks.length - 1].id + 1 
+        } 
         const newTask = {
             "id": index,
             "task": req.body.task
@@ -75,11 +140,18 @@ app.post('/', (req, res) =>{
                 return;
             } 
             res.redirect('/')
-
+        
         })
+    
+     
+        }) 
+        } 
+            
+    }
     })
+    
 
-})
+
 
 app.listen(3001, () =>{
 
